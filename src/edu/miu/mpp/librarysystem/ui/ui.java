774f4452757bc0b6a1e0e4269784d0497340ce2f
ronaldkasaija <reenez.den@gmail.com>
2023-01-24 16:55:18 -0600

    Added adminLoginUi

--- src/edu/miu/mpp/librarysystem/ui/ui.java
+++ src/edu/miu/mpp/librarysystem/ui/ui.java
@@ -1,4 +1,17 @@
 package edu.miu.mpp.librarysystem.ui;
 
 public class ui {
+
+    public static void adminLoginUi() {
+
+        System.out.println(
+                "1. Add new book\n" + "2. Add library member\n"
+                        + "3. Edit library member\n" + "0. Exit" );
+    }
+
+
+    public static void main( String[] args ) {
+
+        adminLoginUi();
+    }
 }
